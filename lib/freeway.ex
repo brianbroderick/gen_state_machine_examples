9462dd420c3e390cb9abef4200b7f99f32ea9f1f
brianbroderick <brianbroderick19@gmail.com>
2018-01-29 15:08:26 -0700

    Freeway example, simplified

--- lib/freeway.ex
+++ lib/freeway.ex
@@ -1,62 +1,63 @@
 defmodule Freeway do
   @moduledoc """
-  Example module for a
-  gen state machine representing a freeway.
-  On this freeway, there's a special lane for cars and another one for trucks.
+  Example module for a gen state machine representing a freeway.
+
+  This freeway has a smart meter that allows cars to enter the freeway
+  on a timer or if too many cars are waiting in that lane.
 
   To call:
   {:ok, pid} = Freeway.start_link
-  GenStateMachine.cast(pid, {:enter, %Vehicle{}})
+  GenStateMachine.cast(pid, {:new, Freeway.car})
   """
 
+  @doc """
+  Passing a list with :state_functions and :state_enter means it will also fire an
+  :enter event.
+  """
+  # use GenStateMachine, callback_mode: [:state_functions, :state_enter]
   use GenStateMachine, callback_mode: :state_functions
 
-  defstruct car: [], truck: []
-
   def init(_) do
-    {:ok, :road, %Freeway{}}
+    {:ok, :red, []}
   end
 
   def start_link do
     GenStateMachine.start_link(__MODULE__, [])
   end
 
-  def road(:cast, {:enter, vehicle}, data) do
-    data = case vehicle_type(vehicle) do
-          "car" -> %{data | car: [vehicle | data.car]}
-          "truck" -> %{data | truck: [vehicle | data.truck]}
-          _ -> IO.puts "Unknown vehicle type"; data
-    end
-    IO.inspect data
+  def red(:cast, {:new, vehicle}, data) do
+    data = [vehicle | data]
 
-    {:keep_state, data}
+    cars = length(data)
+    IO.puts "Number of cars waiting in line: #{to_string(cars)}"
+
+    cond do
+      cars > 5 ->
+        IO.puts "Meter turned green due buildup in traffic."
+        data = enter_freeway(data)
+        {:next_state, :red, data}
+      true ->
+        {:keep_state, data, [{:state_timeout, 3_000, :turn_green}]}
+    end
+  end
+  def red(:enter, _previous_state, _data) do
+    :keep_state_and_data
+  end
+  def red(:state_timeout, :turn_green, data) do
+    IO.puts "Meter turned green due to timeout."
+    data = enter_freeway(data)
+
+    {:next_state, :red, data}
   end
 
-  defp vehicle_type(vehicle) do
-    vehicle.decoded_value["object_type"]
+  def enter_freeway(data) do
+    ### In a real example, you'd likely send the data list somewhere to be processed
+    {count, data} = {length(data), []}
+    IO.puts "#{to_string(count)} cars are entering the freeway."
+    data
   end
 
   def car do
     %Vehicle{}
   end
-
-  def truck do
-    %Vehicle{ decoded_key: "1fmzu67e92uc29655",
-              decoded_value: %{
-                "object_type" => "truck",
-                "current_value" => %{
-                  "color" => "white",
-                  "make" => "Ford",
-                  "model" => "F150",
-                  "year" => "2018",
-                  "mileage" => "6543",
-                  "inserted_at" => 1516746431685129000,
-                  "vin" => "1fmzu67e92uc29655"
-                }
-              }
-            }
-  end
 end
-
-
-
